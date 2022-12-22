8c1a14db6102d8e50056845af71de7e757b386f8
JoungSangJun <gogumi4502@nate.com>
2022-12-22 10:33:09 +0900

    18weeks

---
+++ app/src/main/java/com/example/fastcampus/_04FragmentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fastcampus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class _04FragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_04_fragment)
+    }
+}
