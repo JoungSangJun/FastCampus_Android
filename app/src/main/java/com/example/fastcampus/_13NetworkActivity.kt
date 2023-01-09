8fb4c72af483942253be9f7a784ef88a22b794f0
JoungSangJun <gogumi4502@nate.com>
2023-01-09 17:06:48 +0900

    25weeks

---
+++ app/src/main/java/com/example/fastcampus/_13NetworkActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fastcampus
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class _13NetworkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_13_network)
+        NetworkAsyncTask().execute()
+    }
+}
+
+class NetworkAsyncTask() : AsyncTask<Any?, Any?, Any?>() {
+    override fun doInBackground(vararg params: Any?): Any? {
+        val urlString: String = "http://mellowcode.org/json/students/"
+        val url: URL = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Content-Type", "application/json")
+
+        var buffer = ""
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream,
+                    "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+            Log.d("testt", buffer)
+        }
+        return null
+    }
+}
